210769c7247bf0b570bd7940e6bed2df17fa0221
Salman Yaqoob <sy@in-hq.com>
2017-12-18 11:43:20 +0300

    first commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
 /**
  * Created by Salman on 12/18/2017.
  */
+require('./config/config');
+
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+    console.log(`App is started on port ${port}`);
+});
+
+module.exports = {app};
